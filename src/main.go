1f3296c235aee06c71c83ce104cfe9f65fe86123
Rolin Blake <66855226+rolinmb@users.noreply.github.com>
2024-11-23 08:45:48 -0600

    made it better for yall

--- src/main.go
+++ src/main.go
@@ -1,26 +1,49 @@
 package main
 
 import(
-  "fmt"
-  "log"
-  "os/exec"
+    "fmt"
+    "log"
+    "os"
+    "os/exec"
 )
 
 const (
-  VIDIN = "src/vid_in/dumbliveremixes2_11222024.mp4"
-  OUTNAME = "src/audio_out/dumbliveremixes2_11222024.mp3"
+    VIDEODIR = "vid_in"
+    AUDIODIR = "audio_out"
+    VIDIN = "/dumbliveremixes3_11232024.mp4"
+    OUTNAME = "/dumbliveremixes3_11232024.mp3"
 )
 
+func checkCreateDir(dirName string) error {
+    info, err := os.Stat(dirName)
+    if os.IsNotExist(err) {
+        err := os.MkdirAll(dirName, 0755)
+        if err != nil {
+            return fmt.Errorf("main.go : checkCreateDir() :: ERROR ::: Failed to create directory %s", dirName)
+        }
+        fmt.Println("main.go : CheckCreateDir() :: Created directory %s", dirName)
+    } else if err != nil {
+        return fmt.Errorf("main.go : checkCreateDir() :: ERROR ::: An error occured while checking directory %s", dirName)
+    } else if !info.IsDir() {
+        return fmt.Errorf("main.go : checkCreateDir() :: ERROR :: Path %s exists but it is not a directory", dirName)
+    } else {
+        fmt.Printf("\nmain.go : checkCreateDir() :: Directory %s already exisits\n", dirName)
+    }
+    return nil
+}
+
 func main() {
-  ffmpegCmd := exec.Command(
-    "ffmpeg",
-    "-i", VIDIN,
-    "-vn", "-acodec", "libmp3lame", "-q:a", "4",
-    OUTNAME,
-  )
-  ffmpegOutput, err := ffmpegCmd.CombinedOutput()
-  if err != nil {
-    log.Fatalf("\nmain(): An error occured while running ffmpegCmd: %s\n%v", string(ffmpegOutput), err)
-  }
-  fmt.Printf("\nmain(): Successfully extracted audio from %s and saved as %s with ffmpeg\n\n%s", VIDIN, OUTNAME, string(ffmpegOutput))
+    checkCreateDir(VIDEODIR)
+    checkCreateDir(AUDIODIR)
+    ffmpegCmd := exec.Command(
+        "ffmpeg",
+        "-i", VIDEODIR+VIDIN,
+        "-vn", "-acodec", "libmp3lame", "-q:a", "4",
+        AUDIODIR+OUTNAME,
+    )
+    ffmpegOutput, err := ffmpegCmd.CombinedOutput()
+    if err != nil {
+        log.Fatalf("\nmain(): An error occured while running ffmpegCmd: %s\n%v", string(ffmpegOutput), err)
+    }
+    fmt.Printf("\nmain(): Successfully extracted audio from %s and saved as %s with ffmpeg\n\n%s", VIDEODIR+VIDIN, AUDIODIR+OUTNAME, string(ffmpegOutput))
 }
